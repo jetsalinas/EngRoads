53ad873bd3607ded45be100208f20a43641ee2ea
Jose Enrico Salinas <joseenrico.salinas@gmail.com>
2017-02-17 23:49:48 +0800

    Basic build of simulation with basic probability and map implementation less time dilation.

--- Application/src/Main.java
+++ Application/src/Main.java
@@ -1,10 +1,22 @@
+import Map.RoadQueue;
+import Map.Vehicle;
+
+import java.util.ArrayList;
+import java.util.Queue;
+import java.util.Stack;
 
 /**
  * Created by Jose Salinas on 2/9/2017.
  */
 public class Main {
 
+
+
+
     public static void main(String args[]) {
-        System.out.println("Hello World!");
+
+        Simulator simulator = new Simulator();
+        simulator.simulate();
     }
+
 }
--- Application/src/Map/Vehicle.java
+++ Application/src/Map/Vehicle.java
@@ -1,8 +1,67 @@
 package Map;
 
+import java.math.BigInteger;
+
 /**
  * Created by Jose Salinas on 2/12/2017.
  */
 public class Vehicle {
 
+    long entryTime;
+    long pickupTime;
+    long exitTime;
+    long loopCount;
+    boolean hasPassenger;
+
+    public Vehicle() {
+        entryTime = 0;
+        pickupTime = 0;
+        exitTime = 0;
+        loopCount = 0;
+        hasPassenger = false;
+    }
+
+    public long getEntryTime() {
+        return entryTime;
+    }
+
+    public void setEntryTime(long entryTime) {
+        this.entryTime = entryTime;
+    }
+
+    public long getPickupTime() {
+        return pickupTime;
+    }
+
+    public void setPickupTime(long pickupTime) {
+        this.pickupTime = pickupTime;
+    }
+
+    public long getExitTime() {
+        return exitTime;
+    }
+
+    public void setExitTime(long exitTime) {
+        this.exitTime = exitTime;
+    }
+
+    public long getLoopCount() {
+        return loopCount;
+    }
+
+    public void setLoopCount(long loopCount) {
+        this.loopCount = loopCount;
+    }
+
+    public void loopVehicle() {
+        this.loopCount++;
+    }
+
+    public boolean hasPassenger() {
+        return hasPassenger;
+    }
+
+    public void setHasPassenger(boolean hasPassenger) {
+        this.hasPassenger = hasPassenger;
+    }
 }
